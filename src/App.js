3893d40c10ada8507ee51ee7b79387c0c7dd9856
Ashru-amj <jenad@DESKTOP-BF0E9VK>
2023-10-12 02:28:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+import './App.css';
+import Hero from './Components/Hero';
+import Navigation from './Components/Navigation';
+
+function App() {
+  return (
+    <div>
+     <Navigation/>
+     <Hero/>
+    </div>
+  );
+}
+
+export default App;
